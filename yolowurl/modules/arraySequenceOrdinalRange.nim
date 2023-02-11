40624a0a6e24f72137b42fbe8aca49fe617f5d41
poop <noahedwardhall@gmail.com>
2023-02-11 09:47:40 -0700

    array

--- yolowurl/modules/arraySequenceOrdinalRange.nim
+++ yolowurl/modules/arraySequenceOrdinalRange.nim
@@ -1,55 +1,61 @@
 ##
-## ordinals, array, sequences
+## ordinals, array, sequences, range
 ## ==========================
 ##
-## ordinals: enums, integers, char, bool, subranges
 ## including arrays and sequences as the interface is similar to Ordinals
+##
+##
+## ordinals: enums, integers, char, bool, subranges
+## - only enums and subranges are included in this file
+## - see typeSimple.nim for integers, chars and bool
+##
+## arrays: array, openArray, UncheckedArray
 ##
 #[
   types
-    UncheckedArray[T] array with no bounds checking
-    openArray[T] ptr to the array data and a length field
     array[n, T] fixed-length dimensionally homogeneous
-    seq[T] dynamic-length dimensionally homogeneous
+    openArray[T] ptr to the array data and a length field
     Ordinal[T] generic ordinal type
+    seq[T] dynamic-length dimensionally homogeneous
+    UncheckedArray[T] array with no bounds checking
+    cstringArray
 
-  array procs
-    swapRefsInArray swaps x[N] with y[N] if the elements are refs
-
-  seq procs
+  immutable ops
+    [a .. ^b]	Slice: b is a backwardsIndex (inclusive)
+    [a .. b]	Slice: inclusive
+    [a ..< b]	Slice: excluded upper bound like b == len - 1
     @	Turn an array type into a sequence
-    add	Add an item to the sequence
+    & concat 2 things
+    ord(x)	returns the integer value that is used to represent x's value
+    succ(x[, n]) returns the n'th successor of x
+    pred(x[, n]) opposite of succ, i.e. previous
+    cstringArrayToSeq cstringArray to seq[int]
+
+  mutable ops
+    add	y to x
+    dec(x, n)	decrements x by n; n is an integer
+    dec(x)	decrements x by one
     del	O(1) removal, doesn't preserve the order
     delete	Delete an item while preserving the order of elements (O(n) operation)
-    high (len x) - 1
+    inc(x, n)	increments x by n; n is an integer
+    inc(x)	increments x by one
     insert	Insert an item at a specific position
     newSeq[T](n)	create seq of T with length n, = values to each index instead of add
     newSeq[T](s: seq[T]; n) create seq of T with length n, assigned to var s
     newSeqOfCap	Create a new sequence with zero length and a given capacity
     newSeqUninitialized only available for number types
     pop	Remove and return last item of a sequence
+    swapRefsInArray swaps x[N] with y[N] if the elements are refs
+    setLen increase/truncate the length of something
+    toOpenArray not defined in js targets
 
-
-  ordinal procs
-    [a .. ^b]	Slice: b is a backwardsIndex (inclusive)
-    [a .. b]	Slice: inclusive
-    [a ..< b]	Slice: excluded upper bound like b == len - 1
-    & concat 2 things
-    add	y to x
+  inspection ops
     contains true if y is in x, shortcut for find(a, item) => 0
-    dec(x, n)	decrements x by n; n is an integer (mutates)
-    dec(x)	decrements x by one (mutates)
-    high(x) highest possible value/index
-    inc(x, n)	increments x by n; n is an integer (mutes)
-    inc(x)	increments x by one (mutates)
     len	Return the length
     low(x) lowest possible value/index
-    ord(x)	returns the integer value that is used to represent x's value
-    pred(x, n)	returns the n'th predecessor of x, pred(5) == 4
-    setLen increase/truncate the length of something
-    succ(x, n)	returns the n'th successor of x
-    succ(x)	returns the successor of x
-
+    high (len x) - 1
+    high(x) highest possible value/index
+    varargsLen the number of variadic arguments in x
 ]#
 
 echo "############################ arrays"
@@ -171,3 +177,41 @@ echo level        # --> [on, fastBlink, slowBlink, off]
 echo low(level)   # --> north
 echo len(level)   # --> 4
 echo high(level)  # --> west
+
+
+echo "############################ range"
+# .. Binary slice operator that constructs an inclusive interval
+# b[0 .. ^1] ==  b[0 .. b.len-1] == b[0 ..< b.len]
+# forward: starts at 0
+# backward: start at ^1,
+# range of values from an integer or enumeration type
+# are checked at runtime whenever the value changes
+# valuable for catching / preventing underflows.
+# e.g. Nims natural type: type Natural = range[0 .. high(int)]
+# ^ should be used to guard against negative numbers
+
+# BackwardsIndex returned by ^ (distinct int) values for reverse array access
+const lastFour = ^4
+const lastOne = ^1
+echo "tell me your name ", "my name is noah"[lastFour .. lastOne]
+
+# range[T] for constructing range types
+type
+  MySubrange = range[0..5]
+echo MySubrange
+
+echo "############################ slice"
+# same syntax as slice but different type (Slice) & context
+# collection types define operators/procs which accept slices in place of ranges
+# the operator/proc specify the type of values they work with
+# the slice provides a range of values matching the type required by the operator/proc
+
+# HSlice[T; U] T inclusive lower bound, U inclusive upper bound
+# Slice[T] alias for HSlice[T, T]
+# copied from docs
+var
+  a = "Nim is a programming language"
+  bbb = "Slices are useless."
+echo a[7 .. 12] # --> 'a prog' > forward slice
+bbb[11 .. ^2] = "useful" # backward slice
+echo bbb # --> 'Slices are useful.'
