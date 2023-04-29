1d0f344b42aab7edf8a63ccad98e91cee06dafa7
poop <noahedwardhall@gmail.com>
2023-04-28 22:41:38 -0500

    add link to commit that removed the db stuff

--- src/bookofnim/v2/dbsV2.nim
+++ src/bookofnim/v2/dbsV2.nim
@@ -6,13 +6,14 @@
 ##[
 ## TLDR
 - changes in v2
-  - all the db_ modules have been removed from impure dir
-  - pure/db_common.nim removed
+  - [std/lib db wrappers moved to nimble packages](https://github.com/nim-lang/Nim/commit/9ba07edb2ec7fcdd628cfa7155c4853160ebd5c3#diff-3bd14d078188074c410028847113ceae68865d0ad5b844a27183ef87fbe2fcc3)
 
 links
 -----
 ]##
 
 {.push warning[UnusedImport]:off .}
+
+import std/db_mysql
 
 echo "dbs v2!"
